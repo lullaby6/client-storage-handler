03c590a0ad225d36ac6dfc9537ccdeeba688975c
LucianoBrumer <lucianobrumer5@gmail.com>
2024-08-09 11:09:39 -0300

    refactoring

--- cdn.js
+++ cdn.js
@@ -2,50 +2,23 @@ const csh = {
     local: {
         all: () => localStorage,
         clear: () => localStorage.clear(),
-
         get: key => localStorage.getItem(key),
         set: (key, value) => localStorage.setItem(key, value),
         remove: key => localStorage.removeItem(key),
-
-        exists: key => localStorage.getItem(key) !== null,
-
+        exists: key => key in localStorage,
         keys: () => Object.keys(localStorage),
         values: () => Object.values(localStorage),
         entries: () => Object.entries(localStorage),
     },
-
     cookies: {
-        all: () => {
-            const cookies = document.cookie.split(";").reduce( (ac, cv, i) => Object.assign(ac, {[cv.split('=')[0]]: cv.split('=')[1]}), {})
-
-            delete cookies[undefined]
-            delete cookies[""]
-
-            return cookies
-        },
-        clear: () => document.cookie.split(';').forEach(cookie => document.cookie = cookie + ";expires=" + new Date(0).toUTCString()),
-
-        object: cookies => {
-            csh.cookies.clear()
-            document.cookie = Object.entries(cookies).map(([k, v]) => `${k}=${v}`).join("; ")
-        },
-
-        get: key => csh.cookies.all()[key] ,
-        set: (key, value) => {
-            const cookies = csh.cookies.all()
-            cookies[key] = value
-            csh.cookies.object(cookies)
-        },
-        remove: key => {
-            const cookies = csh.cookies.all()
-            delete cookies[key]
-            csh.cookies.object(cookies)
-        },
-
-        exists: key => csh.cookies.get(key) !== undefined,
-
+        all: () => Object.fromEntries(document.cookie.split(';').map(s => s.trim().split('=')).filter(([k]) => k)),
+        clear: () => document.cookie.split(';').forEach(c => document.cookie = c.split('=')[0] + '=;expires=' + new Date(0).toUTCString()),
+        get: key => csh.cookies.all()[key],
+        set: (key, value) => document.cookie = `${key}=${value}`,
+        remove: key => document.cookie = `${key}=;expires=${new Date(0).toUTCString()}`,
+        exists: key => key in csh.cookies.all(),
         keys: () => Object.keys(csh.cookies.all()),
         values: () => Object.values(csh.cookies.all()),
         entries: () => Object.entries(csh.cookies.all()),
     }
-}
+};
